99d32c2d1a462ab0046d0d96469bd43a4efa939e
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-03-31 16:23:58 +0530

    Set Status and Navigation Bar Colors in Android

--- composeApp/src/androidMain/kotlin/com/axondragonscale/color/stack/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/axondragonscale/color/stack/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.statusBarColor = Color.Black.copy(0.7f).toArgb()
+        window.navigationBarColor = Color.Black.toArgb()
 
         setContent {
             App()
